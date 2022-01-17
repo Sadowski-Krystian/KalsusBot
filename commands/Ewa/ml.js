53bb02ec35f395a4365f81d571257c634a6ea800
Sadowski <krystek.sadowski@gmail.com>
2022-01-18 00:38:25 +0100

    [Version 1.0 fixed]

--- commands/Ewa/ml.js
+++ commands/Ewa/ml.js
@@ -3,10 +3,15 @@ module.exports = {
     description: "Wyświetla spis komend",
     //usage: "help",
     execute: async(message, id, client) =>{
-        let user = message.guild.members.cache.get(id);
-        console.log(id + "tomote");
-        console.log(user.roles);
-        user.roles.add(`845678055434879076`);
+        try{
+            let user = message.guild.members.cache.get(id);
+            console.log(id + "tomote");
+            console.log(user.roles);
+            user.roles.add(`845678055434879076`);
+        }catch(err){
+            console.log(err);
+        }
+        
         
     }
 }
